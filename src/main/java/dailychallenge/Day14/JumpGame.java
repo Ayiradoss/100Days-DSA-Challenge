94bfcc01132e54c6faae9a6ec8635cd2b9ba37ed
Ayira Doss <your_github_email@example.com>
2026-03-01 20:53:09 +0530

    Day 14 Greedy Problems

---
+++ src/main/java/dailychallenge/Day14/JumpGame.java
@@ -0,0 +1,22 @@
+package dailychallenge.Day14;
+
+public class JumpGame {
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+
+        System.out.print(canJump(nums));
+    }
+    public static boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(i > maxReach)
+                return false;
+
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
